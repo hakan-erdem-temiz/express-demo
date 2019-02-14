94f68d6476e75798851cc68c01c74ced265a9dea
Hakan Erdem Temiz <hakanerdemtemiz@gmail.com>
2019-02-14 09:22:06 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
@@ -8,6 +9,21 @@ const courses = [
   { id: 2, name: "course2" },
   { id: 3, name: "course3" }
 ];
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  return Joi.validate(course, schema);
+}
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port}...`));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -19,12 +35,17 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course)
-    // 404
-    res.status(404).send("The course with given ID was not found.");
+    return res.status(404).send("The course with given ID was not found.");
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) {
+    //400 Bad Request
+    return res.status(400).send(error.details[0].message);
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -34,6 +55,29 @@ app.post("/api/courses", (req, res) => {
   res.send(course);
 });
 
-//PORT
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on ${port}...`));
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with given ID was not found.");
+
+  //const result = validateCourse(req.body);
+  const { error } = validateCourse(req.body);
+  if (error) {
+    //400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with given ID was not found.");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
